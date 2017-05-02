fa493b223765f08da95e2123a7a462933b702cf2
Benjamin Neil <ben@neil-concepts.com>
2017-05-01 20:12:10 -0600

    Mod, upgrade, upgrading the deps again

--- src/main/g8/src/main/scala/$organization__packaged$/$name__Camel$.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/$name__Camel$.scala
@@ -7,5 +7,6 @@ import com.twitter.util.Await
 object $name;format="Camel"$ extends App {
     val api: Endpoint[String] = get("hello") { Ok("Hello, World!") }
 
+    println("started http://localhost:8080")
     Await.ready(Http.server.serve(":8080", api.toServiceAs[Text.Plain]))
 }
